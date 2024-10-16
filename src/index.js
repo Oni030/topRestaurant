34275fba1a767b2d685f5859711823a5133738b4
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-10-16 21:59:16 +0200

    Import loadAboutPage() and call it. For dev purposes.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 import { loadHomePage } from "./home.js";
+import { loadAboutPage } from "./about.js";
 
 console.log("JavaScript is running!")
 
 document.addEventListener("DOMContentLoaded", () => {
-    loadHomePage();
+    // loadHomePage();
+    loadAboutPage();
 });
