e26e15b935156c89e88b2a8037802c96d1089220
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-10-16 20:11:28 +0200

    Import loadHomePage from home.js and call it.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./styles.css";
+import { loadHomePage } from "./home.js";
 
 console.log("JavaScript is running!")
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadHomePage();
+});
