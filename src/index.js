62467b92233bef83ce6d79be6f15fb946fea5602
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-10-28 14:08:34 +0100

    Import loadMenuPage() and call it for development.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 import { loadHomePage } from "./home.js";
+import { loadMenuPage } from "./menu.js";
 
 console.log("JavaScript is running!")
 
 document.addEventListener("DOMContentLoaded", () => {
-    loadHomePage();
+    // loadHomePage();
+    loadMenuPage();
 });
