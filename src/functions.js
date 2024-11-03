8812a3e4dd31b6780ec61974d2725a7814704e9b
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-11-03 12:45:34 +0100

    Create makeImage() to simplify image element creation.

--- src/functions.js
+++ src/functions.js
@@ -6,4 +6,14 @@ function makeElement(type, classname, content, parent) {
     return element;
 };
 
-export { makeElement }
+function makeImage(parent, ...objects) {
+    const element = document.createElement("img");
+    objects.forEach((obj) => {
+        for(let key in obj) {
+            element.setAttribute(key, obj[key])
+        };
+    });
+    if (parent !== "") {parent.appendChild(element)};
+};
+
+export { makeElement, makeImage }
