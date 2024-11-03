4ba4dc668a12bc35d9d2ceb86947261625b77ad7
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-11-03 12:44:08 +0100

    Import makeElement() and using it to refactor code.

--- src/home.js
+++ src/home.js
@@ -1,3 +1,4 @@
+import { makeElement } from './functions';
 import smallLogoSrc from './images/logo-white-transparent.png';
 import dish1Src from './images/sichuan_dish2.jpg'
 import dish2Src from './images/sichuan_dish4.jpg'
@@ -6,95 +7,56 @@ function loadHomePage() {
     console.log("loadHomePage function is called");
     const contentContainer = document.querySelector("#content");   
     
-
     // Outside container
-    const homeWrapper = document.createElement("div");
-    homeWrapper.classList.add("section");
-    homeWrapper.classList.add("welcome");
-    homeWrapper.classList.add("hero");
+    const homeWrapper = makeElement("div", "hero", "", contentContainer);
     
-
-    // Spacer
-    const spacer1 = document.createElement("div");
-    const spacer2 = document.createElement("div");
-    spacer1.classList.add("spacer1");
-    spacer2.classList.add("spacer2");
+    // Spacer 1
+    const spacer1 = makeElement("div", "spacer1", "", homeWrapper);
 
     // Panel 1
-    const panel1 = document.createElement("div");
-    panel1.classList.add("panel");
-
+    const panel1 = makeElement("div", "panel", "", homeWrapper);
 
     // Logo Top
-    const smallLogo = document.createElement("img");
+    const smallLogo = makeElement("img", "", "", panel1);
     smallLogo.setAttribute("id", "small-logo");
     smallLogo.setAttribute("src", smallLogoSrc);
     smallLogo.setAttribute("alt", "small-logo");
-    panel1.appendChild(smallLogo);
     
+    // Title Text
+    const titleText = makeElement("h2", "", "Welcome to the Red Dragon", panel1);
     
-    // Title Texts
-    const titleText = document.createElement("h2");
-    titleText.textContent = "Welcome to the Red Dragon"
+    // Subtitle Text
+    const subtitleText = makeElement("h3", "", "home of authentic sichuan comfort food", panel1);
     
-    const subtitleText = document.createElement("h3");
-    subtitleText.textContent = "home of authentic sichuan comfort food"
+    // Spacer 2
+    const spacer2 = makeElement("div", "spacer2", "", homeWrapper);
     
-    panel1.appendChild(titleText);
-    panel1.appendChild(subtitleText);
-
-
     // Panel 2
-    const panel2 = document.createElement("div");
-    panel2.classList.add("panel");
+    const panel2 = makeElement("div", "panel", "", homeWrapper);
 
-    const textBox1 = document.createElement("div");
-    textBox1.classList.add("text-box");
+    const textBox1 = makeElement("div", "text-box", "", panel2);
     
-    const text1 = document.createElement("p")
-    text1.classList.add("text");
-    text1.textContent = "At Red Dragon, we bring the bold and fiery flavors of Sichuan cuisine right to the heart of the city. Family-run and born from our deep passion for food, our restaurant is more than just a place to eat—it's a celebration of togetherness. We believe that the best meals are shared, and nothing brings people together like the rich, authentic taste of Sichuan comfort food."
-    textBox1.appendChild(text1);
+    const text1 = makeElement("p", "text", "At Red Dragon, we bring the bold and fiery flavors of Sichuan cuisine right to the heart of the city. Family-run and born from our deep passion for food, our restaurant is more than just a place to eat—it's a celebration of togetherness. We believe that the best meals are shared, and nothing brings people together like the rich, authentic taste of Sichuan comfort food.", textBox1);
     
-    const imgBox1 = document.createElement("div");
-    const img1 = document.createElement("img");
+    const imgBox1 = makeElement("div", "", "", panel2);
+
+    const img1 = makeElement("img", "", "", imgBox1);
     img1.setAttribute("id", "Dish1");
     img1.setAttribute("src", dish1Src);
-    imgBox1.appendChild(img1);
-    
-    panel2.appendChild(textBox1);
-    panel2.appendChild(imgBox1);
-    
     
     // Panel 3
-    const panel3 = document.createElement("div");
-    panel3.classList.add("panel");
+    const panel3 = makeElement("div", "panel", "", homeWrapper);
     
-    const textBox2 = document.createElement("div");
-    textBox2.classList.add("text-box");
+    const textBox2 = makeElement("div", "text-box", "", panel3);
     
-    const text2 = document.createElement("p");
-    text2.classList.add("text");
-    text2.textContent = "Indulge in classics like Kung Pao Chicken, Mapo Tofu, Dan Dan Noodles, and the signature Sichuan Hot Pot, all prepared with the freshest ingredients and time-honored recipes passed down through generations. Whether you're craving the numbing heat of Sichuan Peppercorns or the savory warmth of our family dishes, Red Dragon is where great food meets great company."
-    textBox2.appendChild(text2);
+    const text2 = makeElement("p", "text", "Indulge in classics like Kung Pao Chicken, Mapo Tofu, Dan Dan Noodles, and the signature Sichuan Hot Pot, all prepared with the freshest ingredients and time-honored recipes passed down through generations. Whether you're craving the numbing heat of Sichuan Peppercorns or the savory warmth of our family dishes, Red Dragon is where great food meets great company.", textBox2);
     
-    const imgBox2 = document.createElement("div");
+    const imgBox2 = makeElement("div", "", "", panel3)
+
     const img2 = document.createElement("img");
     img2.setAttribute("id", "Dish2");
     img2.setAttribute("src", dish2Src);
     imgBox2.appendChild(img2);
-    
-    panel3.appendChild(imgBox2);
-    panel3.appendChild(textBox2);
-    
-
-    // Appending elements
-    homeWrapper.appendChild(spacer1);
-    homeWrapper.appendChild(panel1);
-    homeWrapper.appendChild(spacer2);
-    homeWrapper.appendChild(panel2);
-    homeWrapper.appendChild(panel3);
-    contentContainer.appendChild(homeWrapper);
 };
 
 export { loadHomePage };
