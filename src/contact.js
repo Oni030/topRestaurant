1e12f348f7ebbb012c022019d4309b1a39a23292
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-11-03 11:09:31 +0100

    Import makeElement(). Refactor code to use makeElement().

--- src/contact.js
+++ src/contact.js
@@ -1,39 +1,41 @@
+import { makeElement } from "./functions";
+
 function loadContactPage() {
     console.log("loadBookingPage function is called");
     const contentContainer = document.querySelector("#content"); 
 
-
     // Outside container
-    const bookingWrapper = document.createElement("div");
-    bookingWrapper.classList.add("menu");
-
+    const contactWrapper = makeElement("div", "menu", "", contentContainer);
+    
+    // Panel 1
+    const panel1 = makeElement("div", "menu-title-panel", "", contactWrapper);
 
     // Title Texts
-    const titleText = document.createElement("h2");
-    titleText.textContent = "Reservation"
-
-
-    // Panel 1
-    const panel1 = document.createElement("div");
-    panel1.classList.add("menu-title-panel");
-
-    panel1.appendChild(titleText);
-
-
+    const titleText = makeElement("h2", "", "Contact", panel1);
+    
     // Panel 2
-    const panel2 = document.createElement("div");
-    panel2.classList.add("menu-panel");
-    
-    const bookingContent = document.createElement("div");
-    bookingContent.classList.add("menu-content");
+    const panel2 = makeElement("div", "menu-panel", "", contactWrapper);
 
-    
-    
+    const timesContainer = makeElement("div", "menu-content", "", panel2);
+    const timesTitle = makeElement("h3", "", "Opening Hours", timesContainer)
+    const timesList = makeElement("div", "times-list", "", timesContainer)
+    const monday = makeElement("div", "day", "Monday", timesList);
+    const mondayTime = makeElement("div", "time", "closed", timesList);
+    const tuesday = makeElement("div", "day", "Tuesday", timesList);
+    const tuesdayTime = makeElement("div", "time", "8:00 - 21:00", timesList);
+    const wednesday = makeElement("div", "day", "Wednesday", timesList);
+    const wednesdayTime = makeElement("div", "time", "8:00 - 21:00", timesList);
+    const thursday = makeElement("div", "day", "Thursday", timesList);
+    const thursdayTime = makeElement("div", "time", "8:00 - 21:00", timesList);
+    const friday = makeElement("div", "day", "Firday", timesList);
+    const fridayTime = makeElement("div", "time", "8:00 - 21:00", timesList);
+    const saturday = makeElement("div", "day", "Saturday", timesList);
+    const saturdayTime = makeElement("div", "time", "8:00 - 21:00", timesList);
+    const sunday = makeElement("div", "day", "Sunday", timesList);
+    const sundayTime = makeElement("div", "time", "8:00 - 21:00", timesList);
 
-    // Appending elements
-    bookingWrapper.appendChild(panel1);
-    bookingWrapper.appendChild(panel2);
-    contentContainer.appendChild(bookingWrapper);
+    const contactContainer = makeElement("div", "menu-content");
+    const contactTitle = makeElement("div", "", "Reservation");
 };
 
 export { loadContactPage };
