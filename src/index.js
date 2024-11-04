9d500f317c426fa095b419f8054666a2172a1d3a
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-11-04 15:11:24 +0100

    Add functionality to nav-buttons.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,29 @@ import { loadContactPage } from "./contact.js";
 console.log("JavaScript is running!")
 
 document.addEventListener("DOMContentLoaded", () => {
+    const homeButton = document.getElementById("home-button");
+    const menuButton = document.getElementById("menu-button");
+    const contactButton = document.getElementById("contact-button");
+    const contentContainer = document.querySelector("#content");
+    
     loadHomePage();
-    // loadMenuPage();
-    // loadContactPage();
+
+    function clear() {
+        while(contentContainer.firstChild) {
+            contentContainer.removeChild(contentContainer.firstChild);
+        };
+    };
+
+    homeButton.addEventListener("click", () => {
+        clear();
+        loadHomePage();
+    });
+    menuButton.addEventListener("click", () => {
+        clear();
+        loadMenuPage();
+    });
+    contactButton.addEventListener("click", () => {
+        clear();
+        loadContactPage();
+    });
 });
